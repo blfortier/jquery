11610fdec50773d6d2f60ff88b9cd0108a5b4964
Brittney <brittfort34@gmail.com>
2018-08-15 11:04:54 +0000

    Removing Content

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 /*global $*/
 
-var wrapper = "<div class='wrapper'>";
-var button = $(".button");
-var wrapped = true;
 
-button[0].onclick = function(){
-  if(wrapped) {
-      $("section").unwrap();
-      wrapped = false;
-      button.text("Wrap");
-  } else {
-      $("section").wrapAll(wrapper);
-      wrapped = true;
-      button.text("Unwrap");
-  }
-};
-
-
+$(".button").remove();
+$("#contact img").remove();
