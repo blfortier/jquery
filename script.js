5e5804d14773b15a8fba349c9c5d5e35a30cabd6
Brittney <brittfort34@gmail.com>
2018-08-15 08:54:27 +0000

    jQuery Filters

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 /*global $*/
 
-$("h3").css({border: "3px solid blue"});
+/* $("header nav li:first").css({border: "2px solid red"});
+$("header nav li:last").css({border: "2px solid red"}); */
 
-$(".wrapper").css({border: "3px solid red"});
+/*
+$("#contact ul:first-child").css({border: "2px solid red"});
+$("#contact ul:last-child").css({border: "2px solid red"});  */
 
-$("#clients").css({border: "3px solid yellow"});
+/* $("header nav li:even").css({border: "2px solid blue"}); 
+
+$("header nav li:odd").css({border: "2px solid yellow"}); 
+
+$("section:not('#contact')").css({border: "2px solid green"});
+
+$("#social-nav li:lt(3)").css({border:"2px solid blue"}); 
+$("#social-nav li:gt(2)").css({border:"2px solid blue"});  
+
+$("div[class]").css({border:"2px solid pink"});  
+$("img[alt = quote]").css({border:"2px solid purple"}); */
