3b26f8cde07bea3bdf624c60e69ca9f23afbac5d
Brittney <brittfort34@gmail.com>
2018-08-16 04:29:30 +0000

    CSS with jQuery

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 /*global $*/
 
-console.log($("#contact img").attr("alt"));
+
+$("#social-nav").css({
+    "top" : "-400px",
+    "left" : "150px",
+    "opacity" : "0.5",
+    "border-top" : "45px solid red"
+});
