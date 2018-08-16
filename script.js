c5c8391c3090372000913e4aea32354064090141
Brittney <brittfort34@gmail.com>
2018-08-16 04:55:10 +0000

    Binding & Unbinding Events

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 /*global $*/
-
  var button = $("#lead-banner a");
- 
- button[0].onclick = function() {
+  button[0].onclick = function() {
    $("#points-of-sale").toggleClass("open");
    return false;
  };
+ 
+ 
+
+var myLis = $("#points-of-sale li");
+
+myLis.on("click", function(){
+    $(this).css({"background" : "pink"});
+    myLis.off("click");
+});
