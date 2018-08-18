171db308dce8c35c24c700db667a7f125df52341
Brittney <brittfort34@gmail.com>
2018-08-18 14:36:07 +0000

    Fading Elements In & Out

--- script.js
+++ script.js
@@ -8,16 +8,15 @@
 /*************************************************/
 
 $(document).ready(function(){
- 
-   function complete(){
-      alert("Animation complete")
-    }
     
     $("section > h2").on("click", function() {
-        $(this).animate({"width" : "500px", "height" : "100px"}, 1000, "linear", complete);
-    
-    
-    
+        $(this).fadeTo(200, 0.2)
+               .fadeTo(1000, 0.8)
+               .fadeTo(200, 0.2)
+               .fadeTo(1000, 0.8)
+               .fadeTo(200, 0.2)
+               .fadeTo(1000, 0.8);
+   
   });
 });
 
