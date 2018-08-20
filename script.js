9a28029b040b78ba9acdb5ed3ccd222e383f89e8
Brittney <brittfort34@gmail.com>
2018-08-20 03:56:08 +0000

    Using Plugins

--- script.js
+++ script.js
@@ -5,42 +5,16 @@ button[0].onclick = function() {
  return false;
 };
 
-/*************************************************/
-
-$(document).ready(function() {
- var allQuotes = $("blockquote");
- var currentQuote = 0;
-
- function changeQuote() {
-
-  $(allQuotes[currentQuote]).fadeOut(200, function() {
-     if (currentQuote == allQuotes.length - 1) {
-        currentQuote = 0;
-   } else {
-        currentQuote++;
-   }
-
-   $(allQuotes[currentQuote]).fadeIn(200);
-   
-  });
- }
-
- var quoteTimer = setInterval(changeQuote, 3000);
-
-});
-
 /*******************************************************/
 
+
+
 $(document).ready(function(){
-     
-     var items = $("#points-of-sale li");
-     
-     items.on("click", function(){
-      
-       $(this).find("p").slideToggle(500);
-      
-     });
+   
+   $(".rslides").responsiveSlides();
+   
 });
+
 
 
 
